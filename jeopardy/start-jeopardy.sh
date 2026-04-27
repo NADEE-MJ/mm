bb0016691e72a55ec21f6b724f9478bef5dbd86d
NADEE-MJ <nadeem.maida@gmail.com>
2026-04-27 13:39:43 -0700

    chore: remove config-only folders to replace with git subtrees

--- jeopardy/start-jeopardy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-cd /opt/projects/jeopardy/
-uv run ./j install --sync
-exec uv run ./j s --host 0.0.0.0 --port 3000
--- raddle.teams/start-raddle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-cd /opt/projects/raddle.teams/
-uv run ./rt install --sync
-exec uv run ./rt s --host 0.0.0.0
-
